11ed026b8d8cba0df984d8c8c45a1f821a2601d1
eatgrass <johnny.hwong@hotmail.com>
2017-09-17 21:24:31 +0800

    init

---
+++ src/main/java/me/huangjing/lab/springcloud/ConfigClientApplication.java
@@ -0,0 +1,26 @@
+package me.huangjing.lab.springcloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+	@Value("${foo}")
+	private String foo;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	ResponseEntity hello() {
+		return ResponseEntity.ok(foo);
+	}
+
+}
