7e1147857d391b133a9bf752f03fd40e637f3e9e
eatgrass <johnny.hwong@hotmail.com>
2017-09-18 21:31:42 +0800

    eureka

--- src/main/java/me/huangjing/lab/springcloud/ConfigClientApplication.java
+++ src/main/java/me/huangjing/lab/springcloud/ConfigClientApplication.java
@@ -3,12 +3,15 @@ package me.huangjing.lab.springcloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
 	@Value("${foo}")
